f7b7efcdbcf484a462fd99a47e7bd4bb7a07cbb2
Rensytimsy <rensytimsy03@gmail.com>
2024-01-31 10:32:12 +0300

    project done

--- js_library/script.js
+++ js_library/script.js
@@ -2,28 +2,52 @@ const book_title = document.getElementById("title")
 const book_author = document.getElementById("author")
 const book_pages = document.getElementById("pages")
 const add_book_btn = document.getElementById("btn")
+const books_display_page = document.getElementById("books_page");
+// console.log(books_display_page);
 
 
-let Books = [];
-function my_library(title, author, pages){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    // return JSON.stringify(this.title, this.author, this.pages);
+const all_books = [];
+function Books(title, author, pages){
+  this.title = book_title.value;
+  this.author = book_author.value;
+  this.pages = book_pages.value;
+}
+// const new_book = new Books((book_title, book_author, book_pages));
+
+function add_book(){
+const new_book = new Books((book_title, book_author, book_pages));
+all_books.push(new_book);
+let is_read = document.createElement("button");
+is_read.innerText = "not read";
+
+let allBooksHTML = "";
+all_books.forEach((book) => {
+  allBooksHTML += `<div class="book_card">
+  <p class="book_title">${book.title}</p> 
+  <p class= "book_author">${book.author}</p> 
+  <p class = "book_pages">${book.pages}</p>
+  <button id="is_read">Read</button>
+  </div>`;
+});
+books_display_page.innerHTML = allBooksHTML;
+console.log(all_books);
 }
 
-add_book_btn.addEventListener("click", (e) => {
-    let books_avail = new my_library(title.value, author.value, pages.value);
-    Books.push(JSON.stringify(books_avail));
-    console.log(Books);
-    document.getElementById("books_page").innerHTML = Books;
-    book_title.value = ""
-    book_author.value = ""
-    book_pages.value = ""
-    console.log(Books)
+let del_btn = document.createElement("button");
+del_btn.textContent = "del"
+del_btn.addEventListener("click", (e) => {
+  all_books.forEach((b) => {
+    if(b.title == b.title){
+      b.filter((books) => books.title != books.title);
+    }
+  });
+  console.log("About to delete a book");
 });
 
-function add_book(Books){
-  return console.log(Books);
-}
-add_book(Books);
+add_book_btn.addEventListener("click", (e) => {
+  add_book();
+  book_title.value = " ";
+  book_author.value = " ";
+  book_pages.value = " ";
+});
+console.log(all_books);
